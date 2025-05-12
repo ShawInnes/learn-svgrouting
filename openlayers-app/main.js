01d1b8a89f52953122a0ae357bde1c7269a800aa
Shaw Innes <shaw@immortal.net.au>
2025-05-13 01:32:32 +1000

    feat: add label stles

--- openlayers-app/main.js
+++ openlayers-app/main.js
@@ -6,12 +6,12 @@ import proj4 from "proj4";
 import {register} from 'ol/proj/proj4'; // Import register function
 import Projection from 'ol/proj/Projection';
 import VectorLayer from "ol/layer/Vector";
-import {ZoomSlider, ZoomToExtent} from "ol/control";
-import {Fill, Stroke, Style} from "ol/style";
+import {FullScreen, ZoomSlider, ZoomToExtent} from "ol/control";
 import {floorsGeoJson} from "./data/floors";
 import {obstaclesGeoJson} from "./data/obstacles";
 import {desksGeoJson} from "./data/desks";
 import {roomsGeoJson} from "./data/rooms";
+import {Text, Fill, Stroke, Style} from "ol/style";
 
 let proj4String = `+proj=tmerc +lat_0=-115 +lon_0=102 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs`;
 
@@ -22,23 +22,9 @@ register(proj4);
 
 // Create a new OpenLayers Projection using the registered EPSG code
 const customProjection = new Projection({
-    code: 'EPSG:9999',
-    units: 'm',
-    extent: [-488, -518, 594, 285]
+    code: 'EPSG:9999', units: 'm', extent: [-488, -518, 594, 285]
 });
 
-
-const vectorStyle = [
-    new Style({
-        stroke: new Stroke({
-            color: 'black',
-            width: 3,
-        }),
-        fill: new Fill({
-            color: 'rgba(0, 0, 255, 0.1)',
-        }),
-    }),
-];
 
 function reverseYCoordinates(coordinates) {
     return coordinates.map(coordPair => [coordPair[0], -coordPair[1]]);
@@ -58,49 +44,54 @@ function transformGeoJSON(geoJsonData) {
 const floorsLayer = new VectorLayer({
     source: new VectorSource({
         features: transformGeoJSON(floorsGeoJson),
-    }),
-    style: vectorStyle
+    }), style: {
+        'stroke-color': 'black', 'stroke-width': 4, 'fill-color': 'rgba(28,164,248,0.2)',
+    },
 });
 
 const obstaclesLayer = new VectorLayer({
     source: new VectorSource({
         features: transformGeoJSON(obstaclesGeoJson),
-    }),
-    style: vectorStyle
+    }), style: {
+        'fill-color': 'rgba(168,168,168,0.57)',
+    },
 })
 
 const roomsLayer = new VectorLayer({
     source: new VectorSource({
         features: transformGeoJSON(roomsGeoJson),
-    }),
-    style: vectorStyle
+    }), style: function (feature, resolution) {
+        return new Style({
+            fill: new Fill({
+                color: 'rgba(255, 165, 0, 0.49)', // Orange color with transparency
+            }),
+            stroke: new Stroke({
+                color: '#ff9900',
+                width: 2,
+            }),
+            text: new Text({
+                font: 'Arial',
+                declutterMode: 'obstacle',
+                text: feature.get('name')
+            }),
+        });
+    }
 })
 
 const desksLayer = new VectorLayer({
     source: new VectorSource({
         features: transformGeoJSON(desksGeoJson),
-    }),
-    style: vectorStyle
+    }), style: {
+        'fill-color': 'rgba(236,11,88,0.43)',
+    },
 })
 
 const map = new Map({
     target: 'map',
-    controls: [
-        new ZoomSlider({}),
-        new ZoomToExtent({})
-    ],
-    layers: [
-        floorsLayer,
-        obstaclesLayer,
-        roomsLayer,
-        desksLayer,
-    ],
+    controls: [new FullScreen({}), new ZoomSlider({}), new ZoomToExtent({}),],
+    layers: [floorsLayer, obstaclesLayer, roomsLayer, desksLayer,],
     view: new View({
-        projection: customProjection,
-        center: [102, 115],
-        zoom: 2,
-        minZoom: 1,
-        maxZoom: 5
+        projection: customProjection, center: [102, 115], zoom: 2, minZoom: 1, maxZoom: 5
     }),
 });
 
@@ -108,13 +99,11 @@ const info = document.getElementById('info');
 
 let currentFeature;
 const displayFeatureInfo = function (pixel, target) {
-    const feature = target.closest('.ol-control')
-        ? undefined
-        : map.forEachFeatureAtPixel(pixel, function (feature, layer) {
-            if (layer === desksLayer || layer === roomsLayer) {
-                return feature;
-            }
-        });
+    const feature = target.closest('.ol-control') ? undefined : map.forEachFeatureAtPixel(pixel, function (feature, layer) {
+        if (layer === desksLayer || layer === roomsLayer) {
+            return feature;
+        }
+    });
 
     if (feature) {
         info.style.left = pixel[0] + 10 + 'px';
